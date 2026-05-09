8b808203c3786d1509c403c8304be822f3d48715
wenyunchuan <wenyunchuan@kylinsec.com.cn>
2026-05-09 10:22:26 +0800

    添加LICENSE
    Signed-off-by: wenyunchuan <wenyunchuan@kylinsec.com.cn>

--- linux/ctl.c
+++ linux/ctl.c
@@ -1,3 +1,18 @@
+/**
+ * Copyright (c) 2025 ~ 2026 KylinSec Co., Ltd.
+ * cyanfs is licensed under Mulan PSL v2.
+ * You can use this software according to the terms and conditions of the Mulan PSL v2.
+ * You may obtain a copy of Mulan PSL v2 at:
+ *          http://license.coscl.org.cn/MulanPSL2
+ * THIS SOFTWARE IS PROVIDED ON AN "AS IS" BASIS, WITHOUT WARRANTIES OF ANY KIND,
+ * EITHER EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO NON-INFRINGEMENT,
+ * MERCHANTABILITY OR FIT FOR A PARTICULAR PURPOSE.
+ * See the Mulan PSL v2 for more details.
+ *
+ * Author: huhuikai <huhuikai@kylinsec.com.cn>
+ * Author: wenyunchuan <wenyunchuan@kylinsec.com.cn>
+ * Author: yuanzhu <yuanzhu@kylinsec.com.cn>
+*/
 #include "cyanfs.h"
 
 static DEFINE_MUTEX(cyanfs_ctl_lock);
